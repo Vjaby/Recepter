b5234a8e1f795e8981cc8af1adf28cc2fc1ac698
Flares1075 <134272138+Flares1075@users.noreply.github.com>
2024-04-22 21:33:11 +0200

    Deleting added

--- Recepter/MainWindow.xaml.cs
+++ Recepter/MainWindow.xaml.cs
@@ -20,19 +20,19 @@ namespace Recepter
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		List<Ingredient> ingredients = new List<Ingredient>
+		List<Ingredient> Ingredients = new List<Ingredient>
 		{
-			new Ingredient("potat", 10, "mg"),
+			new Ingredient("potat", 10, "mg"), // for easier testing, REMOVE LATER
 			new Ingredient("ham", 20, "kg")
 		};
 
-		List<Step> steps = new List<Step>
+		List<Step> Steps = new List<Step>
 		{
 			new Step(1, "do this"),
 			new Step(2, "do that"),
 		};
 
-		List<Note> notes = new List<Note>
+		List<Note> Notes = new List<Note>
 		{
 			new Note("Oh boy"),
 			new Note("Hmmmm")
@@ -41,9 +41,9 @@ namespace Recepter
 		public MainWindow()
 		{
 			InitializeComponent();
-			IngredientsItemsControl.ItemsSource = ingredients;
-			StepsItemsControl.ItemsSource = steps;
-			NotesItemsControl.ItemsSource = notes;
+			IngredientsItemsControl.ItemsSource = Ingredients;
+			StepsItemsControl.ItemsSource = Steps;
+			NotesItemsControl.ItemsSource = Notes;
 
 		}
 
@@ -94,31 +94,77 @@ namespace Recepter
 		{
 			MessageBox.Show("saveAs");
 		}
-		#endregion
+        #endregion
 
-		private void AddIngredientButton_Click(object sender, RoutedEventArgs e)
+        #region Add Buttons
+        private void AddIngredientButton_Click(object sender, RoutedEventArgs e)
 		{
-			ingredients.Add(new Ingredient("", 0, ""));
+			Ingredients.Add(new Ingredient("", 0, ""));
 			IngredientsItemsControl.ItemsSource = null;
-			IngredientsItemsControl.ItemsSource = ingredients; //if it works...
+			IngredientsItemsControl.ItemsSource = Ingredients; //if it works...
 		}
 
 		private void AddStepButton_Click(object sender, RoutedEventArgs e)
 		{
-			steps.Add(new Step(steps.Count + 1, ""));
+			Steps.Add(new Step(Steps.Count + 1, ""));
 			StepsItemsControl.ItemsSource = null;
-			StepsItemsControl.ItemsSource = steps; //if it works...
+			StepsItemsControl.ItemsSource = Steps; //if it works...
 		}
 
 		private void AddNoteButton_Click(object sender, RoutedEventArgs e)
 		{
-			notes.Add(new Note(""));
+			Notes.Add(new Note(""));
 			NotesItemsControl.ItemsSource = null;
-			NotesItemsControl.ItemsSource = notes; //if it works...
+			NotesItemsControl.ItemsSource = Notes; //if it works...
 		}
+		#endregion
+
+		#region Delete Buttons
+		private void NoteDeleteButton_Click(object sender, RoutedEventArgs e)
+		{
+			//I have never seen this syntax (though that isnt saying much)
+			//anyway, taken from here: https://stackoverflow.com/questions/16342885/access-other-xaml-controls-data-in-another-controls-event?rq=3
+			Button btn = (Button)sender;
+			Note btnDataContext = (Note)btn.DataContext;
+
+			/* a bit of history
+			at first I thought I would use RemoveAt for this, but then
+			this simpler thing came out at random out of IntelliSense/Code
+
+			just a reminder to thank our Microsoft overlords
+			*/
+			Notes.Remove(btnDataContext);
+			NotesItemsControl.ItemsSource = null;
+			NotesItemsControl.ItemsSource = Notes;
+		}
+
+		private void StepDeleteButton_Click(object sender, RoutedEventArgs e)
+		{
+			Button btn = (Button)sender;
+			Step btnDataContext = (Step)btn.DataContext;
+
+			Steps.Remove(btnDataContext);
+			for (int i = 0; i < Steps.Count(); i++)
+			{
+				Steps[i].StepId = i + 1;
+			}
+			StepsItemsControl.ItemsSource = null;
+			StepsItemsControl.ItemsSource = Steps;
+		}
+
+		private void IngredientDeleteButton_Click(object sender, RoutedEventArgs e)
+		{
+			Button btn = (Button)sender;
+			Ingredient btnDataContext = (Ingredient)btn.DataContext;
+
+			Ingredients.Remove(btnDataContext);
+			IngredientsItemsControl.ItemsSource = null;
+			IngredientsItemsControl.ItemsSource = Ingredients;
+		} 
+		#endregion
 	}
 
-	public class Ingredient
+    public class Ingredient
 	{
         public Ingredient(string name, int amount, string unit)
         {
@@ -134,13 +180,13 @@ namespace Recepter
 
 	public class Step
 	{
-		public Step(int stepNumber, string stepContent)
+		public Step(int stepId, string stepContent)
 		{
-			StepNumber = stepNumber; //this probably isnt the way
+			StepId = stepId;
 			StepContent = stepContent;
 		}
 
-		public int StepNumber { get; set; }
+		public int StepId { get; set; }
 		public string StepContent { get; set; }
 
 	}
